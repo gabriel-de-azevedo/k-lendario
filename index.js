59cfa3b06a29fad00eb50a6ca382bec8ec236595
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-09-05 17:37:05 -0300

    feat: add HTMX

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.post("/clicked", (_, res) => {
+  res.sendFile(__dirname + "/components/button.html");
 });
